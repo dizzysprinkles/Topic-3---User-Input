db28dcac6c020c2b22d1634002bdf898e0cf9b3f
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-07 13:54:38 -0500

    Finished Greetings

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string firstName;
+            int age, currentYear, yearBorn;
+            currentYear = DateTime.Now.Year;
+
+            //Greetings! 
+            Console.WriteLine("Hello! What's your name?  ");
+            firstName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"{firstName} is a wonderful name! Now, how old are you?");
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine();
+
+            yearBorn = currentYear - age;
+            Console.WriteLine($"Oh my god {firstName}! So you were born in {yearBorn}?! I feel so old!");
+            Console.WriteLine();
+
+
+
         }
     }
 }
